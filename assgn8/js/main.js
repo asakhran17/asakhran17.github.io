d70c68036ad17036c0164a9de41ebde73f891d16
Amrita <asakhran@andrew.cmu.edu>
2019-12-04 00:27:02 -0500

    asg8
    asg8

---
+++ assgn8/js/main.js
@@ -0,0 +1,136 @@
+//import lozad from 'lozad'
+
+
+jQuery(document).ready(function( $ ) {
+
+		// Menu settings
+		$('#menuToggle, .menu-close').on('click', function(){
+			$('#menuToggle').toggleClass('active');
+			$('body').toggleClass('body-push-toleft');
+			$('#theMenu').toggleClass('menu-open');
+		});
+
+		// Smooth scroll for the menu and links with .scrollto classes
+	  $('.smoothscroll').on('click', function(e) {
+	    e.preventDefault();
+	    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      if (target.length) {
+
+	        $('html, body').animate({
+	          scrollTop: target.offset().top
+	        }, 1500, 'easeInOutExpo');
+	      }
+	    }
+			$('body').toggleClass('body-push-toleft');
+			$('#theMenu').toggleClass('menu-open');
+	  });
+
+    lozad('.lozad', {
+    load: function(el) {
+        el.src = el.dataset.src;
+        el.onload = function() {
+            el.classList.add('fade')
+        }
+    }
+}).observe()
+
+
+});
+
+//Lazy load images on the page
+//const observer = lozad();
+//observer.observe();
+
+// lozad('.lozad', {
+//     load: function(el) {
+//         el.src = el.dataset.src;
+//         el.onload = function() {
+//             el.classList.add('fade')
+//         }
+//     }
+// }).observe()
+
+// Credit to Tobias Ahlin free text animation code
+anime.timeline({loop: false})
+
+  .add({
+    targets: '.ml5 .line',
+    opacity: [0.5,1],
+    scaleX: [0, 1],
+    easing: "easeInOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml5 .line',
+    duration: 600,
+    easing: "easeOutExpo",
+    translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
+  })
+  .add({
+    targets: '.ml5 .ampersand',
+    opacity: [0,1],
+    scaleY: [0.5, 1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=600'
+  })
+  .add({
+    targets: '.ml5 .letters-left',
+    opacity: [0,1],
+    translateX: ["0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=300'
+  })
+  .add({
+    targets: '.ml5 .letters-right',
+    opacity: [0,1],
+    translateX: ["-0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=600'
+  })
+
+  //.add({
+    //targets: '.ml5',
+    //opacity: 0,
+    //duration: 1000,
+    //easing: "easeOutExpo",
+    //delay: 1000
+  //})
+
+  ;
+
+
+ // Wrap every letter in a span
+var textWrapper = document.querySelector('.ml1 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml1 .letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: (el, i) => 70 * (i+1)
+  }).add({
+    targets: '.ml1 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: '-=875',
+    delay: (el, i, l) => 80 * (l - i)
+  })
+  /* .add({
+    targets: '.ml1',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  })*/ 
+  ;
+
